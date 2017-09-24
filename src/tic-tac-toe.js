4f291f33327c4e2ba08e66cefc56a386b9a24901
Olga1984 <noa84@tut.by>
2017-09-24 21:07:58 +0300

    TicTacToe: implement constructor, metod getCurrentPlayerSymbol

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,38 @@
 class TicTacToe {
     constructor() {
-
+        this.field = [[],[],[]];
+        this.currentPlayerSumbol = 'x';
+        this.winner = null;
+        this.move = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+//should return correct player symbol
+        return this.currentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+//should update game state correctly
     }
 
     isFinished() {
-
+//should return return true or false
     }
 
     getWinner() {
-
+//should return winner of the game or null
     }
 
     noMoreTurns() {
-
+//should return true if game field is full and false otherwise
     }
 
     isDraw() {
-
+//should return false if game is not finished or there is a winner, and true if it is a draw
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+//should return correct field value
     }
 }
 
